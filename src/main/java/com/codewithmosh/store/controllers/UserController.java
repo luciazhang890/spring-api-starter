8231e6ed5003bd96d931197538ef0469e9521c16
Lucy Zhang <luciazhang890@gmail.com>
2025-06-13 12:17:49 +0930

    Implement dynamic routes with path variables for users

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,20 +3,28 @@ import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/users")//used at the class level to define a common prefix for the entire controller.
 public class UserController {
     private final UserRepository userRepository;
 
 //    @RequestMapping("/users")
 //    //method: GET, POST, PUT, DELETE
 
-    @GetMapping("/users")
+    @GetMapping
     public Iterable<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    //get one customer's info
+    @GetMapping("/{id}" )
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
